9eefc160a0753765c3921db870b4561cadb7eec6
djami <djamilatraore31@gmail.com>
2023-09-25 16:01:08 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
+    <link href="http://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css" rel="stylesheet">
+    <link rel="stylesheet" href="styles.css">
+    <title>Document</title>
+    <!-- icone bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</head>
+<body>
+    <navbar>
+    <?php include ("menu.php") ?>
+        <!-- <div class="logo">
+            <img src="1.webp" alt="">
+         <p>Burkina Faso : Pays des Hommes intègres</p>
+         </div>
+        <div class="icons">
+        <a href="#"><i class="ri-facebook-fill"></i></a>
+        <a href="#"><i class="ri-instagram-fill"></i></a>
+        <a href="#"><i class="ri-youtube-fill"></i></a>
+        <a href="#"><i class="ri-twitter-fill"></i></a>
+        <h3>Langues</h3>
+        </div>
+        </div>
+    </navbar>
+    <section class="container">
+    <div class="img">
+    <img class="B" src="2.webp" alt="">
+   
+    </div>
+    <div>
+     <p class="A">Office National du Tourisme Burkinabe</p>
+     </div>
+</section>
+<section class="C">
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">Accueil</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="Actualites.php">Actualites</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="Partenaires.php">Partenaires</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="Contact.php">Contact</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="Recherche" placeholder="Recherche" aria-label="Recherche">
+        <button class="btn btn-outline-success" type="submit">Recherche</button>
+      </form>
+    </div>
+  </div>
+</nav>
+</section> -->
+
+<header>
+<section class="D">
+<div class="E">
+<h1>Les cascades de Banfora</h1>
+</div>
+</section>
+</header>
+<section class="F">
+<div class="G">
+<p>Accueil  > <span>Les cascades de Banfora</span></p>
+</div>
+</section>
+<section class="H">
+<div class="I">
+<h1>LES CASCADES DE BANFORA</h1>
+</div>
+<div class="J">
+<img src="4.webp" alt="">
+<p class="K">Infos pratiques <br><span>1000F CFA pour les <br> étrangers et 500F CFA</span></p>
+</div>
+<div class="M">
+<p>Situées à environ 12 km au nord-ouest de Banfora, elles tiennent leur nom de la commune de Karfiguéla. Elles constituent <br>  l'un des sites touristiques les plus importants au Burkina Faso. La région des Cascades tire son nom de ces chutes d'eau.</p>
+<p>Banfora, ville frontalière de la Côte d’Ivoire dispose d’un climat doux et d’une végétation comparable à celle des pays <br> côtiers permettant de le nommé sans en abuser la Côte d’Azur du Burkina. Mais au sommet d’une colline, se laissent admirer <br> les fameuses cascades qui font la fierté de tout Banfora.</p>
+<p>Les cascades de Karfiguéla ou cascades de Banfora sont une série de cascades le long du fleuve Comoé au sud-ouest du <br> Burkina Faso. Elles sont situées à environ 12 km au nord-ouest de Banfora et constituent l’un des sites touristiques les plus <br> importants au Burkina Faso. La région des Cascades tire son nom de ces cascades. Dans la chaleur de ce mois de mai, <br> entendre le son de l’eau qui coule au loin est en soi une délivrance. Une dizaine de baignoires naturelles et de bains à <br> remous remplis d’une eau limpide et glacée vous y attendent.</p>
+<p>La visite des lieux est payante : 1000F CFA pour les étrangers et 500F CFA pour les visiteurs nationaux. La seule stricte <br> interdiction est de passer la nuit sur le site.</p>
+</div>
+</section>
+<section class="N">
+<div class="O">
+<P>Partagez:</P>
+</div>
+<div class="P">
+<a href="#"><i class="ri-facebook-fill"></i></a>
+<a href="#"><i class="ri-instagram-fill"></i></a>
+<a href="#"><i class="ri-twitter-fill"></i></a> <br><br><br>
+<button>Voir la gelirie d'images</button>
+</div>
+</section>
+
+<section class="omni">
+    <section class="footer">
+    <?php include ("footer.php") ?>
+
+    <!-- <div class="Q">
+    <img src="2.webp" alt="">
+    <p class="T">Burkina Faso</p>
+    <p class="T">Pays des Hommes intègres</p>
+    <p class="T">Terre d'accueil et de traditions,</p>
+    <p class="T">Pays de culture</p>
+    </div>
+    <div>
+    <p class="R">Contact</p>
+    <div class="S">
+    <i class="bi bi-egg-fill"></i>
+    <P >01 B.P. 1311 Ouagadougou <br>01 Burkina Faso</P>
+    </div>
+    <div class="V">
+    <i class="bi bi-telephone-fill"></i>
+    <p>+226 31 19 59/60</p>
+    </div>
+    <div class="W">
+    <i class="bi bi-envelope"></i> 
+    <p>info@ontb.bf</p>
+    </div>
+   <div class="X">
+   <i class="bi bi-facebook"></i>
+   <i class="bi bi-instagram i"></i>
+   <i class="bi bi-youtube i"></i>
+   <i class="bi bi-twitter i"></i>
+   </div>
+    </div>
+    <div>
+    <p class="Z">Annuaire officiel</p>
+    <div class="a">
+    <i class="bi bi-check"></i>
+    <p> La présidence</p>
+    </div>
+    <div class="b">
+    <i class="bi bi-check"></i>
+    <p> Le gouvernement</p>
+    </div>
+    <div class="c">
+    <i class="bi bi-check"></i>
+    <p> Le SIG</p>
+    </div>
+    <div class="d">
+    <i class="bi bi-check"></i>
+    <p> Le service public</p>
+    </div>
+    <div class="e">
+    <i class="bi bi-check"></i>
+    <p> L'annuaire des institutions</p>
+    </div>
+    </div>
+    
+    
+    <div>
+    <p class="f">Liens utiles</p>
+    <div class="g">
+    <i class="bi bi-check"></i>
+    <p> Le Ministère de la Culture</p>
+    </div>
+     <div class="h">
+    <i class="bi bi-check"></i>
+    <p> Le Musée national</p>
+    </div>
+    <div class="j">
+    <i class="bi bi-check"></i>
+    <p> Investir au Burkina</p>
+    </div>
+    <div class="k">
+    <i class="bi bi-check"></i>
+    <p> Burkina24</p>
+    </div>
+    <div class="l">
+    <i class="bi bi-check"></i>
+    <p> Documents</p>
+    </div> 
+    </div>
+    
+    </section>
+    <div class="m">
+    <P class="o">Propulsé par. FasoCompute.</P>
+    <div class="n">
+    <P>ONTB © 2023 Tous droits réservés   -   Mentions Légales..</P>
+    </div>
+    </div>
+    </section> -->
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+</body>
+</html>
